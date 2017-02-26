6c58eda79943241124f107e7ac55db2309e06e91
Paul Plaza <pool_09pp@hotmail.com>
2017-02-26 14:29:00 -0500

    deplaza

---
+++ SwParqueadero/SwParqueadero.Negocio/Mantenimiento/LogicaMarca.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SwParqueadero.AccesoDatos;
+
+
+namespace SwParqueadero.Negocio.Mantenimiento
+{
+    public class LogicaMarca
+    {
+        private DbParqueoEntities dc = new DbParqueoEntities();
+
+        public List<TBL_MARCA> Lista()
+        {
+            //jola
+            return dc.TBL_MARCA.ToList();
+        }
+    }
+}
